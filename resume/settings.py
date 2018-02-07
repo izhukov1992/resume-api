8570ef8bfe39188221ecf18cd9f3a482498e1166
Ilya Zhukov <izhukov1992@gmail.com>
2018-02-08 00:55:15 +0300

    Use PostgreSQL database

--- resume/settings.py
+++ resume/settings.py
@@ -83,12 +83,16 @@ WSGI_APPLICATION = 'resume.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
+DATABASES = {  
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': 5432,
     }
-}
+} 
 
 
 # Password validation
